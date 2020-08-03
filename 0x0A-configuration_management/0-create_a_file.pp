f8681cf2dff8228e099c24068f544e648941b3d1
Duvan <duvanjarin@gmail.com>
2020-08-03 15:47:57 -0500

    fix task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,7 +2,7 @@
 
 file { '/tmp/holberton':
     ensure  => file,
-    mode    => '744',
+    mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
     content => 'I love Puppet',
