94bc9b1fb988911d7e282e106ba0fe57cf2cdb87
Duvan <duvanjarin@gmail.com>
2020-08-14 18:50:12 -0500

    add task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,31 @@
+#Add a custom HTTP header with Puppet
+
+exec { 'update':
+  command => '/usr/bin/apt-get -y update',
+}
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['update']
+}
+file_line { 'redirect':
+  ensure   => 'present',
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'server_name _;',
+  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
+}
+file_line { 'header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => 'add_header X-Served-By "$HOSTNAME";',
+  require => Package['nginx'],
+}
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+  require => Package['nginx'],
+}
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
