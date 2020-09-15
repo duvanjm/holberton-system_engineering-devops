01c69d52ad46b8de12859f24b8cb2602593d1db2
Duvan <duvanjarin@gmail.com>
2020-09-14 20:22:20 -0500

    fix task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Write a Python script that, using this
-REST API, for a given employee ID, returns
-information about his/her TODO list progress."""
+REST API, for a given employee ID"""
 
 import requests
 import sys
