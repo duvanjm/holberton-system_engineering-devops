eadd64338015fb8bcb47272e12f8ad43eb63aae6
Duvan <duvanjarin@gmail.com>
2020-09-14 20:04:45 -0500

    fix task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,25 +4,35 @@ REST API, for a given employee ID, returns
 information about his/her TODO list progress."""
 
 import requests
+from sys import argv
 import sys
 import json
 
 
-if __name__ == "__main__":
+def _request(val, task):
+    """
+    makes employee request and returns json dict response
+    """
+    url = 'https://jsonplaceholder.typicode.com'
+    info = '{}{}{}'.format(url, val, task)
+    return requests.get(info).json()
 
-    def _request(val, task):
-        """reques for user"""
-        url = 'https://jsonplaceholder.typicode.com'
-        info = "{}{}{}".format(url, val, task)
-        return requests.get(info).json
 
-    def user_(task):
-        """request for user"""
-        user = _request("/users/", task)
-        todo = _request("/todos/?userId=", task)
-        done = [i.get("title") for i in todo if i.get("complete")]
-        all = len(todo)
-        print('Employee {} is done with tasks({}/{}):'.format(
-            user.get('name'), len(done), all))
-        for i in done:
-            print("\t {}".format(t))
+def user_(task):
+    """
+    makes request for info about employee todo list, then prints
+    """
+    user = _request('/users/', task)
+    todo = _request('/todos/?userId=', task)
+    done = [i.get('title') for i in todo if i.get('completed')]
+    all = len(todo)
+    print('Employee {} is done with tasks({}/{}):'.format(
+        user.get('name'), len(done), all))
+    for i in done:
+        print('\t {}'.format(i))
+
+
+if __name__ == '__main__':
+    """main"""
+    if len(sys.argv) > 1 and sys.argv[1].isdigit():
+        user_(sys.argv[1])
