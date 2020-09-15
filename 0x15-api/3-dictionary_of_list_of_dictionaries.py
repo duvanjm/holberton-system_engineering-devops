730c341aaa890796398014894f515938194ed5d8
Duvan <duvanjarin@gmail.com>
2020-09-14 21:00:45 -0500

    fix task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Write a Python script that, using this
 REST API, for a given employee ID"""
 
@@ -6,34 +7,16 @@ import requests
 import json
 
 
-def _request(val, task):
-    """reques for user"""
-    url = 'https://jsonplaceholder.typicode.com'
-    info = '{}{}{}'.format(url, val, task)
-    return requests.get(info).json()
-
-
-def user_():
-    """reques for user"""
-    employee = {}
-    _url = 'https://jsonplaceholder.typicode.com/users/'
-    user = requests.get(_url).json()
-    for u in user:
-        task = str(u.get('id'))
-        user_name = u.get('username')
-        todo = _request('/todos/?userId=', task)
-        all_tasks = [
-            {'username': user_name,
-             'task': i.get('title'),
-             'completed': i.get('completed')}
-            for i in todo
-        ]
-        employee[task] = all_tasks
-    filename = 'todo_all_employees.json'
-    with open(filename, 'w') as f:
-        f.write(json.dumps(employee, sort_keys=True))
-
-
-if __name__ == '__main__':
-    """main"""
-    user_()
+if __name__ == "__main__":
+    req = requests.get('https://jsonplaceholder.typicode.com/users')
+    users = req.json()
+    req = requests.get('https://jsonplaceholder.typicode.com/todos')
+    tasks = req.json()
+    dic = {str(d.get('id')): [{'username': d.get('username'),
+                               'task': i.get('title'), 'completed':
+                               i.get('completed')}
+                              for i in tasks
+                              if i.get('userId') == d.get('id')]
+           for d in users}
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(dic, f)
