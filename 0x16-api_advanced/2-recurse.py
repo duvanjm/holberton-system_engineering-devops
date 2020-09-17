87b36db053d1371f93b8078bf2036dfa1c82717b
Duvan <duvanjarin@gmail.com>
2020-09-17 18:05:18 -0500

    fix query task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -10,18 +10,17 @@ def recurse(subreddit, hot_list=[], after=""):
     """returns a list containing the titles of all
     hot articles for a given subreddit"""
     headers = {"User-Agent": "Holberton"}
-    url = "https://www.reddit.com/r/{}/hot.json?after{}".format(
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(
                        subreddit, after)
     req = requests.get(url, headers=headers)
 
     if req.status_code != 200:
-        print(None)
         return
     redit = req.json().get("data").get("children")
     for chil in redit:
         hot_list.append(chil.get("data").get("title"))
 
-    after = req.json().get('data').get('after')
+    after = req.json().get("data").get("after")
     if after is None:
         return hot_list
     else:
