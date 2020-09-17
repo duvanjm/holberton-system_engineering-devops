42b3162961b657a944a024efd63dc720dc0dd537
Duvan <duvanjarin@gmail.com>
2020-09-17 13:28:19 -0500

    add task 0 and readme

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Write a function that queries the Reddit API
+and returns the number of subscribers"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers"""
+    headers = {"User-Agent": "Holberton"}
+
+    req = requests.get("https://www.reddit.com/r/{}/about.json".format(
+                       subreddit), headers=headers).json()
+    if req.get("data").get("subscribers"):
+        return req.get("data").get("subscribers")
+    else:
+        return 0
