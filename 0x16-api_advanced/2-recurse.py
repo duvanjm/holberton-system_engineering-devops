4c89af42ece0eb6f0230a5eeb02b3eb8f697f681
Duvan <duvanjarin@gmail.com>
2020-09-17 17:57:45 -0500

    add task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""recursive function that queries the Reddit API
+and returns a list containing the titles of all hot
+articles for a given subreddit."""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """returns a list containing the titles of all
+    hot articles for a given subreddit"""
+    headers = {"User-Agent": "Holberton"}
+    url = "https://www.reddit.com/r/{}/hot.json?after{}".format(
+                       subreddit, after)
+    req = requests.get(url, headers=headers)
+
+    if req.status_code != 200:
+        print(None)
+        return
+    redit = req.json().get("data").get("children")
+    for chil in redit:
+        hot_list.append(chil.get("data").get("title"))
+
+    after = req.json().get('data').get('after')
+    if after is None:
+        return hot_list
+    else:
+        return recurse(subreddit, hot_list, after)
